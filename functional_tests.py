24919df20113560731849dccc0bd6fd70d0c4052
sergesever <sergesever@gmail.com>
2016-06-10 17:08:44 +0300

    Use unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,38 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
+    def test_can_start_and_retrieve_a_list(self):
+        # open url
+        self.browser.get('http://localhost:8000')
+
+        # check title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+        # enter a to-do item invitation
+
+        # type 'first' to-do
+
+        # update page, with to-do item in the list
+
+        # add 'another' to-do item
+
+        # update page showing both items in the list
+
+        # generate unique URL for user
+
+        # visit hte URL - to-do list is here
+
+        # quit app
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
