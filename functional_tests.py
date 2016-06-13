4d3b1a11b39e2b157de9b133a7d64dbcccc6a42e
sergesever <sergesever@gmail.com>
2016-06-13 16:39:24 +0300

    UPDATE functional test, now checks to-do input

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -16,15 +17,29 @@ class NewVisitorTest(unittest.TestCase):
 
         # check title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # enter a to-do item invitation
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # type 'first' to-do
+        inputbox.send_keys('Buy peacock feathers')
 
         # update page, with to-do item in the list
+        inputbox.send_keys(Keys.enter)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # add 'another' to-do item
+        self.fail('Finish the test')
 
         # update page showing both items in the list
 
