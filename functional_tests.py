4a4f770f389c5626681812f9dfd0eaa47facf514
sergesever <sergesever@gmail.com>
2016-06-14 19:33:30 +0300

    REFACTOR functional tests: ADD helper function

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_and_retrieve_a_list(self):
         # open url
         self.browser.get('http://localhost:8000')
@@ -40,17 +45,9 @@ class NewVisitorTest(unittest.TestCase):
 
 # debug
         import time
-        time.sleep(5)
+        time.sleep(3)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #    any(row.text == '1: Buy peacock feathers' for row in rows),
-        #    'New item did not appear in table, its text was:\n%s' % table.text
-        # )
-        self.assertIn('1: Buy peacock feathers',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # add another to-do item
 
@@ -59,13 +56,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
 # debug
-        time.sleep(5)
+        time.sleep(3)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock feathers to make a fly',
-             [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # update page showing both items in the list
         self.fail('Finish the test')
