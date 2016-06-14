4b8cb9f374b1ab7def9a69f127ccc61d21de08f9
sergesever <sergesever@gmail.com>
2016-06-14 19:00:00 +0300

    UPDATE home page template; ADD new tests to retrieve a list

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-### pdb
-import pdb
+
+# pdb
+# import pdb
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -33,20 +34,41 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         # update page, with to-do item in the list
-### pdb
+# pdb
 #        pdb.set_trace()
         inputbox.send_keys(Keys.ENTER)
+
+# debug
+        import time
+        time.sleep(5)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New item did not appear in table'
+        # self.assertTrue(
+        #    any(row.text == '1: Buy peacock feathers' for row in rows),
+        #    'New item did not appear in table, its text was:\n%s' % table.text
+        # )
+        self.assertIn('1: Buy peacock feathers',
+            [row.text for row in rows]
         )
 
-        # add 'another' to-do item
-        self.fail('Finish the test')
+        # add another to-do item
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+# debug
+        time.sleep(5)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Use peacock feathers to make a fly',
+             [row.text for row in rows]
+        )
 
         # update page showing both items in the list
+        self.fail('Finish the test')
 
         # generate unique URL for user
 
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request, 'home.html')
+    return render(request, 'home.html',
+            {'new_item_text': request.POST.get('item_text', '')},
+    )
