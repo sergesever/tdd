c234d36debb8da5de7a974c818b7022857920eaa
sergesever <sergesever@gmail.com>
2016-06-15 12:12:48 +0300

    ADD redirect after POST, ADD show all items feature

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+# from django.http import HttpResponse
+from lists.models import Item
 
 
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request, 'home.html',
-            {'new_item_text': request.POST.get('item_text', '')},
+    if request.method == 'POST':
+        # new_item_text = request.POST['item_text']
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    # else:
+        # new_item_text = ''
+
+    # item = Item()
+    # item.text = request.POST.get('item_text', '')
+    # item.save()
+
+    items = Item.objects.all()
+
+    return render(
+        request,
+        'home.html',
+        {'items': items},
     )
