7ea20d8ad81df3b6524ca046df8d013e88a60fcb
sergesever <sergesever@gmail.com>
2016-06-15 16:36:42 +0300

    ADD tests for new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -37,18 +37,20 @@ class NewVisitorTest(LiveServerTestCase):
             'Enter a to-do item'
         )
 
-        # type 'first' to-do
+        # user Edith types 'first' to-do
         inputbox.send_keys('Buy peacock feathers')
 
-        # update page, with to-do item in the list
+        # update page, take to a new url with to-do item in the list
 # pdb
 #        pdb.set_trace()
         inputbox.send_keys(Keys.ENTER)
 
+        edith_list_url = self.browser.current_url
 # debug
         import time
         time.sleep(3)
 
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # add another to-do item
@@ -64,11 +66,37 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # update page showing both items in the list
-        self.fail('Finish the test')
+        # new user visit:
+        ## meta-comment: start new browser session for new user
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # new user Francis starts to type his own list:
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.sendKeys('Buy milk')
+        inputbox.sendKeys(Keys.ENTER)
+# debug
+        time.sleep(3)
+
+        # Francis gets his own url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        selfAssertEqual(francis_list_url, edith_list_url)
+
+        # there is no trace of previous user list
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        # self.fail('Finish the test')
 
         # generate unique URL for user
 
-        # visit hte URL - to-do list is here
+        # visit the URL - to-do list is here
 
         # quit app
 
