9dbb80fda9cab48fe6b11d2854fa9342974c4092
sergesever <sergesever@gmail.com>
2016-06-20 12:53:27 +0300

    ADD FT for layout/styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -97,5 +97,27 @@ class NewVisitorTest(LiveServerTestCase):
 
         # quit app
 
+    def test_layout_and_styling(self):
+        # go to homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # check if the input box is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+            )
+
+        # start a new list check if it is centered
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+            )
+
 # if __name__ == '__main__':
 #    unittest.main(warnings='ignore')
