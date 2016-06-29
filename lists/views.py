d90bb4a5dd2b1073bb291f24a476db84b493c6ca
sergesever <sergesever@gmail.com>
2016-06-29 14:43:11 +0300

    USE form.save() method to save list items

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 # from django.http import HttpResponse
 from lists.forms import ItemForm
-from lists.models import List, Item
+from lists.models import List
 # from django.core.exceptions import ValidationError
 
 
@@ -16,7 +16,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(
         request,
@@ -29,7 +29,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
